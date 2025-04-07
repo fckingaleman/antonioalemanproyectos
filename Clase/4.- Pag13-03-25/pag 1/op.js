82aa290ea5a173869959d5b88c2e49d06c874f7d
fckingaleman <antoniolopezaleman9@gmail.com>
2025-04-06 22:37:10 -0600

    Delete Clase directory

--- Clase/4.- Pag13-03-25/pag 1/op.js
+++
@@ -1,4 +0,0 @@
-var are = ["LOAA080421HDFPLNA0","Antonio Lopez Aleman","16.11","Ciudad de Mexico","Queretaro Numero 22"];
-
-alert("Pagina que guarda y muestra datos");
-alert("Datos: \nCURP: "+are[0]+"\nNombre: "+are[1]+"\nEdad en años y meses: "+are[2]+"\nEstado de Nacimiento: "+are[3]+"\nEstado en el que se radica: "+are[4]);
